171cfbd10cf529c75bc617b7632808402f1788ad
Hiromipaw <silvia@nopressure.co.uk>
2014-10-14 16:37:49 +0200

    updates

--- app/controllers/api/v1/graphs_controller.rb
+++ app/controllers/api/v1/graphs_controller.rb
@@ -20,16 +20,26 @@ class Api::V1::GraphsController < ApplicationController
 
   def up
     category = params[:category] ? params[:category] : "Main_topic_classifications"
-    @category = Category.where(:cat_title => category.capitalize).first
-    if @category
-      @links = Link.where(:cl_sortkey => category.upcase)
-      render :json => @links, each_serializer: LinkSerializer, root: @category.cat_title.downcase
+    category = category.encode(Encoding.find('ASCII'), encoding_options)
+    @links = Link.where(['cl_sortkey LIKE ?', "%#{category.upcase}%"]).limit(15)
+    if @links
+      render :json => @links, each_serializer: LinkSerializer, root: category
     else
       render :json => {:error => {:text => "404 Not found", :status => 404}}
     end
   end
 
   private
+
+  def encoding_options
+    {
+      :invalid           => :replace,  # Replace invalid byte sequences
+      :undef             => :replace,  # Replace anything not defined in ASCII
+      :replace           => '',        # Use a blank for those replacements
+      :universal_newline => true       # Always break lines with \n
+    }
+  end
+
 
   def graph_params
     params.require(:category).permit(:cl_to, :cl_type)
